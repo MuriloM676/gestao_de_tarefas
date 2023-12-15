94d716108c268dc601bcc80f0aec2b97b0cc54d6
murilo <murilom676@gmail.com>
2023-12-14 22:07:00 -0300

    Add opção concluir tarefa e salvar lista de tarefas em .txt

--- main.py
+++ main.py
@@ -1,33 +1,39 @@
-import tarefas
+# main.py
+
+import tasks_module as tm
 import tkinter as tk
 from tkinter import simpledialog, messagebox
-
 
 def adicionar():
     titulo = simpledialog.askstring("Adicionar Tarefa", "Digite o título da tarefa:")
     prazo = simpledialog.askstring("Adicionar Tarefa", "Digite o prazo da tarefa (dd/mm/aaaa):")
-    resultado = tarefas.adicionar_tarefa(titulo, prazo)
+    resultado = tm.adicionar_tarefa(titulo, prazo)
     messagebox.showinfo("Resultado", resultado)
-
 
 def alterar():
     indice = simpledialog.askinteger("Alterar Tarefa", "Digite o índice da tarefa que deseja alterar:")
     novo_titulo = simpledialog.askstring("Alterar Tarefa", "Digite o novo título da tarefa:")
     novo_prazo = simpledialog.askstring("Alterar Tarefa", "Digite o novo prazo da tarefa (dd/mm/aaaa):")
-    resultado = tarefas.alterar_tarefa(indice, novo_titulo, novo_prazo)
+    resultado = tm.alterar_tarefa(indice, novo_titulo, novo_prazo)
     messagebox.showinfo("Resultado", resultado)
 
-
 def listar():
-    lista = tarefas.listar_tarefas()
+    lista = tm.listar_tarefas()
     messagebox.showinfo("Lista de Tarefas", lista)
-
 
 def deletar():
     indice = simpledialog.askinteger("Deletar Tarefa", "Digite o índice da tarefa que deseja deletar:")
-    resultado = tarefas.deletar_tarefa(indice)
+    resultado = tm.deletar_tarefa(indice)
     messagebox.showinfo("Resultado", resultado)
 
+def concluir():
+    indice = simpledialog.askinteger("Concluir Tarefa", "Digite o índice da tarefa que deseja marcar como concluída:")
+    resultado = tm.concluir_tarefa(indice)
+    messagebox.showinfo("Resultado", resultado)
+
+def salvar():
+    resultado = tm.salvar_tarefas()
+    messagebox.showinfo("Resultado", resultado)
 
 def menu():
     root = tk.Tk()
@@ -38,17 +44,18 @@ def menu():
         2: alterar,
         3: listar,
         4: deletar,
-        5: exit
+        5: concluir,
+        6: salvar,
+        7: exit
     }
 
     while True:
         escolha = simpledialog.askinteger("Menu",
-                                          "Escolha uma opção:\n1. Adicionar Tarefa\n2. Alterar Tarefa\n3. Listar Tarefas\n4. Deletar Tarefa\n5. Sair")
+                                          "Escolha uma opção:\n1. Adicionar Tarefa\n2. Alterar Tarefa\n3. Listar Tarefas\n4. Deletar Tarefa\n5. Concluir Tarefa\n6. Salvar Tarefas\n7. Sair")
         if escolha in opcoes:
             opcoes[escolha]()
         else:
             messagebox.showwarning("Opção inválida", "Escolha uma opção válida.")
-
 
 if __name__ == "__main__":
     menu()
---
+++ tasks_module.py
@@ -0,0 +1,54 @@
+# tasks_module.py
+
+tarefas = []  # Lista vazia para armazenar as tarefas
+
+
+def adicionar_tarefa(titulo, prazo):
+    if titulo and prazo:
+        nova_tarefa = {'Título': titulo, 'Prazo': prazo, 'Status': 'Pendente'}
+        tarefas.append(nova_tarefa)
+        return "Tarefa adicionada com sucesso!"
+    else:
+        return "Erro ao adicionar a tarefa. Preencha o título e o prazo."
+
+
+def alterar_tarefa(indice, novo_titulo, novo_prazo):
+    if indice >= 0 and indice < len(tarefas):
+        tarefas[indice]['Título'] = novo_titulo
+        tarefas[indice]['Prazo'] = novo_prazo
+        return "Tarefa alterada com sucesso!"
+    else:
+        return "Erro ao alterar a tarefa. Índice inválido."
+
+
+def listar_tarefas():
+    if not tarefas:
+        return "Não há tarefas cadastradas."
+
+    lista = "Lista de tarefas:\n"
+    for i, tarefa in enumerate(tarefas):
+        lista += f"Índice: {i}, Título: {tarefa['Título']}, Prazo: {tarefa['Prazo']}, Status: {tarefa['Status']}\n"
+    return lista
+
+
+def deletar_tarefa(indice):
+    if indice >= 0 and indice < len(tarefas):
+        del tarefas[indice]
+        return "Tarefa deletada com sucesso!"
+    else:
+        return "Erro ao deletar a tarefa. Índice inválido."
+
+
+def concluir_tarefa(indice):
+    if indice >= 0 and indice < len(tarefas):
+        tarefas[indice]['Status'] = 'Concluída'
+        return "Tarefa marcada como concluída!"
+    else:
+        return "Erro ao concluir a tarefa. Índice inválido."
+
+
+def salvar_tarefas():
+    with open("tarefas.txt", "w") as arquivo:
+        for tarefa in tarefas:
+            arquivo.write(f"Título: {tarefa['Título']}, Prazo: {tarefa['Prazo']}, Status: {tarefa['Status']}\n")
+        return "Tarefas salvas com sucesso!"
